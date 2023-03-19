8059444563455ef5b3ddb98b60193c262ad08f8e
Ridwan NR <rahmannurridwan@student.telkomuniversity.ac.id>
2023-03-19 23:41:46 +0700

    “menambahkan class DataGeneric

--- TP Mod 5/Program.cs
+++ TP Mod 5/Program.cs
@@ -1,2 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
-
+internal class Program
+{
+    public class DataGeneric<T>
+    {
+        private T data;
+        public DataGeneric(T data)
+        {
+            this.data = data;
+        }
+        public void PrintData()
+        {
+            Console.WriteLine("Data yang tersimpan adalah: " + data);
+        }
+    }
+}
